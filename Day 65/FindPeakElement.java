cee6ab726cb7e27e50e7d1c60b2993f4369938ac
Gufran Khan <gufran3004@gmail.com>
2025-10-07 14:08:19 +0530

    Day 65 added

---
+++ Day 65/FindPeakElement.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/find-peak-element/description/
+
+public class FindPeakElement {
+  public static void main(String[] args) {
+    int[] arr = { 0, 1, 0 };
+    System.out.println(peakIndexInMountainArray(arr));
+  }
+
+  static int peakIndexInMountainArray(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      if (arr[mid] > arr[mid + 1]) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  }
+}
